cb70654e8849e491a68d3473221e8e05ce85b497
alexshavlovsky <ctzn@tut.by>
2020-03-24 14:11:33 +0300

    initial commit

---
+++ src/main/java/com/ctzn/springmongoreactivechat/SpringMongoReactiveChatApplication.java
@@ -0,0 +1,13 @@
+package com.ctzn.springmongoreactivechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMongoReactiveChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMongoReactiveChatApplication.class, args);
+	}
+
+}
