a08a9b340dcb78435ce36ae6312b5ea4a47874e5
alexshavlovsky <ctzn@tut.by>
2020-03-26 14:19:32 +0300

    configure mongo driver to connect to a remote docker hosted mongo server

--- src/main/java/com/ctzn/springmongoreactivechat/configuration/MongoConfig.java
+++ src/main/java/com/ctzn/springmongoreactivechat/configuration/MongoConfig.java
@@ -2,6 +2,7 @@ package com.ctzn.springmongoreactivechat.configuration;
 
 import com.mongodb.reactivestreams.client.MongoClient;
 import com.mongodb.reactivestreams.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
@@ -10,13 +11,29 @@ import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRep
 @EnableReactiveMongoRepositories("com.ctzn.springmongoreactivechat.repository")
 class MongoConfig extends AbstractReactiveMongoConfiguration {
 
+    @Value("${spring.data.mongodb.host}")
+    String host;
+
+    @Value("${spring.data.mongodb.port}")
+    String port;
+
+    @Value("${spring.data.mongodb.username}")
+    String username;
+
+    @Value("${spring.data.mongodb.password}")
+    String password;
+
+    @Value("${spring.data.mongodb.database}")
+    String database;
+
     @Override
     protected String getDatabaseName() {
-        return "spring-chat";
+        return database;
     }
 
     @Override
     public MongoClient reactiveMongoClient() {
-        return MongoClients.create();
+        String uri = String.format("mongodb://%s:%s@%s:%s", username, password, host, port);
+        return MongoClients.create(uri);
     }
 }
