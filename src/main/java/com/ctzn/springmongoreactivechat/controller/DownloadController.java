2b92f73fb806fabc13d55c18ca1fcaf0ab8bc67d
alexshavlovsky <ctzn@tut.by>
2020-05-01 15:02:24 +0300

    refactor download controller: use html post method instead of get method

--- src/main/java/com/ctzn/springmongoreactivechat/controller/DownloadController.java
+++ src/main/java/com/ctzn/springmongoreactivechat/controller/DownloadController.java
@@ -9,9 +9,9 @@ import org.springframework.http.ZeroCopyHttpOutputMessage;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
 import java.io.File;
@@ -23,22 +23,31 @@ public class DownloadController {
 
     private Logger LOG = LoggerFactory.getLogger(DownloadController.class);
 
-    @GetMapping("/files/{fileName}")
-    public Mono<Void> downloadByWriteWith(@PathVariable String fileName, ServerHttpResponse response, ServerHttpRequest request) {
-        HttpEvent event = HttpEvent.fromServerHttpRequest(request);
-        ZeroCopyHttpOutputMessage zeroCopyResponse = (ZeroCopyHttpOutputMessage) response;
-        File file = Paths.get("uploaded_files").resolve(fileName).toFile();
-        if (file.exists()) {
-            response.getHeaders().setContentType(MediaType.APPLICATION_OCTET_STREAM);
-            return zeroCopyResponse.writeWith(file, 0, file.length())
-                    .doOnSuccess(part -> LOG.info("f->[{}] {}", event.getRemoteHost(), fileName));
-        } else {
-            DataBuffer dataBuffer = response.bufferFactory().allocateBuffer();
-            dataBuffer.write(fileName.getBytes());
-            response.setRawStatusCode(404);
-            response.getHeaders().setContentType(MediaType.TEXT_PLAIN);
-            return zeroCopyResponse.writeWith(Mono.just(dataBuffer))
-                    .doOnSuccess(part -> LOG.error("fx [{}] {}", event.getRemoteHost(), fileName));
-        }
+    @PostMapping("/files/")
+    public Mono<Void> getFileById(ServerWebExchange serverWebExchange, ServerHttpResponse response, ServerHttpRequest request) {
+        return serverWebExchange.getFormData().flatMap(formData -> {
+                    HttpEvent event = HttpEvent.fromServerHttpRequest(request);
+                    String fileName = formData.getFirst("id");
+                    if (fileName == null) {
+                        LOG.error("f? [{}]", event.getRemoteHost());
+                        response.setRawStatusCode(400);
+                        return Mono.empty();
+                    }
+                    ZeroCopyHttpOutputMessage zeroCopyResponse = (ZeroCopyHttpOutputMessage) response;
+                    File file = Paths.get("uploaded_files").resolve(fileName).toFile();
+                    if (file.exists()) {
+                        response.getHeaders().setContentType(MediaType.APPLICATION_OCTET_STREAM);
+                        return zeroCopyResponse.writeWith(file, 0, file.length())
+                                .doOnSuccess(part -> LOG.info("f->[{}] {}", event.getRemoteHost(), fileName));
+                    } else {
+                        DataBuffer dataBuffer = response.bufferFactory().allocateBuffer();
+                        dataBuffer.write(fileName.getBytes());
+                        response.setRawStatusCode(404);
+                        response.getHeaders().setContentType(MediaType.TEXT_PLAIN);
+                        return zeroCopyResponse.writeWith(Mono.just(dataBuffer))
+                                .doOnSuccess(part -> LOG.error("fx [{}] {}", event.getRemoteHost(), fileName));
+                    }
+                }
+        );
     }
 }
