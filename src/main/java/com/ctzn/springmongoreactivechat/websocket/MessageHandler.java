f753deba9b174b9a13accac4fee3633ac1ba068b
alexshavlovsky <ctzn@tut.by>
2020-05-29 12:59:38 +0300

    refactor the message handler

--- src/main/java/com/ctzn/springmongoreactivechat/websocket/MessageHandler.java
+++ src/main/java/com/ctzn/springmongoreactivechat/websocket/MessageHandler.java
@@ -3,9 +3,9 @@ package com.ctzn.springmongoreactivechat.websocket;
 import com.ctzn.springmongoreactivechat.domain.DomainMapper;
 import com.ctzn.springmongoreactivechat.domain.IncomingMessage;
 import com.ctzn.springmongoreactivechat.domain.Message;
-import com.ctzn.springmongoreactivechat.service.DirectBroadcastService;
 import com.ctzn.springmongoreactivechat.service.BroadcastMessageService;
 import com.ctzn.springmongoreactivechat.service.ChatBrokerService;
+import com.ctzn.springmongoreactivechat.service.DirectBroadcastService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -36,28 +36,30 @@ public class MessageHandler implements WebSocketHandler {
     public Mono<Void> handle(WebSocketSession session) {
         String sessionId = session.getId();
 
-        Flux<WebSocketMessage> outgoingMessages = chatBroker.addClient(sessionId)
-                .concatWith(chatBroker.getBroadcastTopic()
-                        .mergeWith(broadcastMessageService.getTopic())
-                        .mergeWith(directBroadcastService.getTopic()))
-                .map(mapper::asJson)
-                .doOnNext(json -> LOG.trace("==>[{}] {}", sessionId, json))
-                .map(session::textMessage);
-
-        Flux<Message> incomingMessages = session.receive()
+        Mono<Void> input = session.receive()
                 .map(WebSocketMessage::getPayloadAsText)
                 .doOnNext(rawText -> LOG.info("<--[{}] {}", sessionId, rawText))
                 .map(rawText -> mapper.fromJson(rawText, IncomingMessage.class))
                 .doOnNext(message -> {
                     if ("updateMe".equals(message.getType())) chatBroker.updateClient(sessionId, message);
-                    if ("setTyping".equals(message.getType())) directBroadcastService.send(Message.newText(session, message));
+                    if ("setTyping".equals(message.getType()))
+                        directBroadcastService.send(Message.newText(session, message));
                 })
                 .filter(message -> "msg".equals(message.getType()) || "richMsg".equals(message.getType()))
                 .map(message -> Message.newText(session, message))
                 .flatMap(broadcastMessageService::saveMessage)
-                .doFinally(sig -> chatBroker.removeClient(sessionId));
+                .doFinally(sig -> chatBroker.removeClient(sessionId))
+                .then();
 
-        return session.send(outgoingMessages).and(incomingMessages);
+        Flux<String> source = chatBroker.addClient(sessionId)
+                .concatWith(chatBroker.getBroadcastTopic()
+                        .mergeWith(broadcastMessageService.getTopic())
+                        .mergeWith(directBroadcastService.getTopic()))
+                .map(mapper::asJson)
+                .doOnNext(json -> LOG.trace("==>[{}] {}", sessionId, json));
+
+        Mono<Void> output = session.send(source.map(session::textMessage));
+
+        return Mono.zip(input, output).then();
     }
-
 }
